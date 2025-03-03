a602b21c71ed8a642bc6481463571ca528d70fdd
minthwayhtut <minthwayhtut568@gmail.com>
2025-03-03 15:36:58 +0100

    First release of the project

---
+++ script.js
@@ -0,0 +1,6 @@
+var map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/bright/style.json?key=insert_your_key_here',
+    center: [8.5456, 47.3739],
+    zoom: 11
+})
